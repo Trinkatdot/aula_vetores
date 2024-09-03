9778ecc253d6b2f7ddece19197a6411a7f6b7334
Trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2024-09-02 21:07:10 -0300

    feat: exercicio1 correção

--- ex1.js
+++ ex1.js
@@ -1,1 +1,13 @@
 //Crie um algoritmo que leia um vetor de 5 números inteiros e mostre-os.
+
+let numero = [];
+numero [0] = prompt("iforme o 1° numero");
+numero [1] = prompt("iforme o 1° numero");
+numero [2] = prompt("iforme o 1° numero");
+numero [3] = prompt("iforme o 1° numero");
+numero [4] = prompt("iforme o 1° numero");
+numero [5] = prompt("iforme o 1° numero");
+let mensagem =
+    `${numero[0]}, ${numero[1]}, ${numero[2]}, ${numero[3]} e ${numero[4]} `;
+
+alert(mensagem);
