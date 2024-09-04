4076f29c0a6debc8aef5be973f1c66685dcc0569
Trinkatdot <Akenathoncicero20@>
2024-09-04 18:55:28 -0300

    feat: ex 4 e 5

--- ex4.js
+++ ex4.js
@@ -4,12 +4,16 @@ mostre o salário líquido de todos, considerando 12% de INSS a descontar
 let salario = [];
 let mensagem = "";
 let desconto = 0.12;
-for (let i = 0; i < 4; i += 1) {
-    let salario = prompt(parseFloat("informe o salario"));
-    salario [i] = liquido;
+
+//pedir salarios
+for (let i = 0; i < 5; i ++) {
+    let bruto = parseFloat(prompt("informe o seu salario bruto:"));
+    salario[i] = bruto;
 }
-for (j = 0; j < 4; j++) {
-    let liquido = (salario) - (desconto);
-    mensagem = `salario o salario liquido é R$${liquido.toFixed(2)}`
+//calcular descontos
+for (let j = 0; j < 5; j++) {
+    let todosbrutos = salario[j];
+    let todosliquidos = todosbrutos * (1 - desconto);
+    mensagem += `o salario liquido de ${j + 1}é: R$ ${todosliquidos.toFixed(2)}\n`;
 }
 alert(mensagem);
--- ex5.js
+++ ex5.js
@@ -2,3 +2,18 @@
 vetor. Armazene os números pares no vetor PAR e os números IMPARES 
 no vetor impar. Deve-se no final mostrar os três vetores
 */
+let numeros = [];
+let mensagem = "";
+let par = [];
+let impar = [];
+
+for (let i = 0; i < 4; i++) {
+    let numero = parseInt(prompt("insira um numero:"));
+    if (numero % 2 === 0) {
+        par.push(numero);
+    } else {
+        impar.push(numero);
+    }
+    mensagem = ("Todos os numeros: " + numero) + ("Números pares: " + par) + ("Números ímpares: " + impar);
+}
+alert (mensagem);
